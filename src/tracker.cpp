6af50ed0e3feda2926fb3b36f36df1722877790a
Nicola Pasta <nicola.pasta@gmail.com>
2014-12-03 21:40:28 +0100

    Tidy-up: change name

--- src/tracker.cpp
+++ src/tracker.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char * argv[])
 {
-    ros::init(argc, argv, "localizator");
+    ros::init(argc, argv, "tracker");
     ros::NodeHandle handle("~");
     ros::spin();
     return 0;
