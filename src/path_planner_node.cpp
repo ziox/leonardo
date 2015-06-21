ab108c5daa45ef69c11de012e39db15119d1d520
Nicola Pasta <nicola.pasta@gmail.com>
2015-06-21 20:13:39 +0200

    Refactor

--- src/path_planner_node.cpp
+++ src/path_planner_node.cpp
@@ -9,7 +9,8 @@
 struct PathPlannerNode
 {
     PathPlannerNode(ros::NodeHandle& nh)
-        : set_point_topic(nh.advertise<geometry_msgs::Point>("set_point", 10))
+        : tick(MaxTick)
+        , set_point_topic(nh.advertise<geometry_msgs::Point>("set_point", 10))
         , command_topic(nh.subscribe<std_msgs::String>("command", 10, &PathPlannerNode::onCommand, this))
     {
         the_tf_setpoints[0] = tf::Vector3(0.0, 0.0, 1.0);
@@ -30,28 +31,17 @@ struct PathPlannerNode
 
     void run()
     {
-        const MotionProfile& profile = SinProfile{};
-
         ros::Rate loop_rate(10); // Hz
         while (ros::ok())
         {
-            double percent = 1.0;
-            double tau  = 3.14 * the_tau / TAU_MAX;
-            if (the_tau < TAU_MAX)
+            if (tick <= MaxTick)
             {
-                percent = profile.position(1.0 * the_tau / TAU_MAX);
+                actual = start + (end - start) * profile.position(1.0 * tick / MaxTick);
+                publishSetPoint();
+                ++tick;
             }
 
-            actual = start + (end - start) * percent;
-            the_tau = the_tau < TAU_MAX ? the_tau + 1 : TAU_MAX;
-
-            geometry_msgs::Point cmd;
-            cmd.x = actual.x();
-            cmd.y = actual.y();
-            cmd.z = actual.z();
-            set_point_topic.publish(cmd);
-
-            publishSetPoint();
+            broadcastSetPoint();
 
             ros::spinOnce();
             loop_rate.sleep();
@@ -67,13 +57,21 @@ private:
             int index = atoi(&command[6]);
             end = the_tf_setpoints[index];
             start = actual;
-            the_tau = 0;
+            tick = 0;
         }
     }
 
     void publishSetPoint()
     {
-        static tf::TransformBroadcaster broadcaster;
+        geometry_msgs::Point cmd;
+        cmd.x = actual.x();
+        cmd.y = actual.y();
+        cmd.z = actual.z();
+        set_point_topic.publish(cmd);
+    }
+
+    void broadcastSetPoint()
+    {
         broadcaster.sendTransform(
             tf::StampedTransform(
                 tf::Transform(tf::createIdentityQuaternion(), actual),
@@ -82,17 +80,18 @@ private:
                 "/the_drone"));
     }
 
-    ros::Subscriber command_topic;
-    ros::Publisher set_point_topic;
-
-    tf::Vector3 the_tf_setpoints[10];
+    int tick;
+    static constexpr int MaxTick = 100;
+    SinProfile profile;
 
     tf::Vector3 start;
     tf::Vector3 actual;
     tf::Vector3 end;
+    tf::Vector3 the_tf_setpoints[10];
 
-    const int TAU_MAX = 100;
-    int the_tau = 100;
+    ros::Subscriber command_topic;
+    ros::Publisher set_point_topic;
+    tf::TransformBroadcaster broadcaster;
 };
 
 
